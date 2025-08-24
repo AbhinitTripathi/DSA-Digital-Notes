068f2b10261082d162d0314680367c53845ee673
Abhinit Tripathi <abhinit.tripathi07@gmail.com>
2025-08-24 11:18:53 +0530

    add 24 august 2025 POTD

---
+++ LeetCode Dailys/24-08-25 Longest Subarray of 1's After Deleting One Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = 0, cnt = 0;
+
+        int ans = -1e9;
+        while(r < n) {
+            if(nums[r] == 0) {
+                cnt++;
+            }
+
+            while(cnt > 1) {
+                if(nums[l] == 0) cnt--;
+                l++;
+            }
+
+            ans = max(ans, r-l+1);
+            r++;
+        }
+
+        return ans-1;
+    }
+};
